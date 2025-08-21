a0d5bc52907f09b02d54c9d5f49ffa92f8fa0fe0
Debora <debora.roldanlara@gmail.com>
2025-08-21 17:00:48 -0300

    subindo estilos gerais do projeto

---
+++ wefit-ecommerce/src/styles/GlobalStyles.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #2F2E41;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    outline: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+    
+  ul {
+    list-style: none;
+  }
+`;
+
