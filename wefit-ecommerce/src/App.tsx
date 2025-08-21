6757ca5ed1df97a168afcadd097481b1780eeb4a
Debora <debora.roldanlara@gmail.com>
2025-08-21 17:02:05 -0300

    Configuração para uso do react-router-dom

--- wefit-ecommerce/src/App.tsx
+++ wefit-ecommerce/src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './contexts/CartContext';
+import { GlobalStyles } from './styles/GlobalStyles';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Success from './pages/Success';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CartProvider>
+        <GlobalStyles />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </CartProvider>
+    </Router>
   );
 }
 
